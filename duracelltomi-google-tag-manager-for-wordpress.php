a5de7d20047199006e5d4b1badba0ee95a3da35e
duracelltomi <duracelltomi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-03 10:04:13 +0000

    Updated version number
    git-svn-id: https://plugins.svn.wordpress.org/duracelltomi-google-tag-manager/trunk@798017 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duracelltomi-google-tag-manager-for-wordpress.php
+++ duracelltomi-google-tag-manager-for-wordpress.php
@@ -1,14 +1,14 @@
 <?php 
 /*
 Plugin Name: DuracellTomi's Google Tag Manager for Wordpress
-Version: 0.2
+Version: 0.3
 Plugin URI: http://www.duracelltomi.com/google-tag-manager-for-wordpress/
 Description: The first Google Tag Manager plugin for WordPress with business goals in mind
 Author: Thomas Geiger
 Author URI: http://www.duracelltomi.com/
 */
 
-define( 'GTM4WP_VERSION',    '0.2' );
+define( 'GTM4WP_VERSION',    '0.3' );
 define( 'GTM4WP_PATH',       plugin_dir_path( __FILE__ ) );
 define( 'GTM4WP_TEXTDOMAIN', 'gtm4wp-lang' );
 
