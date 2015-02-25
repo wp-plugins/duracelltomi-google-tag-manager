091b6ec4f9fbd76608dd6ed35d878770a49729fc
duracelltomi <duracelltomi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 08:54:11 +0000

    Wrong GTM container code output when changing default dataLayer variable name
    git-svn-id: https://plugins.svn.wordpress.org/duracelltomi-google-tag-manager/trunk@1099061 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/frontend.php
+++ public/frontend.php
@@ -389,7 +389,7 @@ function gtm4wp_get_the_gtm_tag() {
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
 new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=\'' . $gtm4wp_datalayer_name . '\'?\'&l=\'+l:\'\';j.async=true;j.src=
+j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
 \'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,\'script\',\'' . $gtm4wp_datalayer_name . '\',\'' . $gtm4wp_options[ GTM4WP_OPTION_GTM_CODE ] . '\');</script>
 <!-- End Google Tag Manager -->';
